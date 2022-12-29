5bb2e5a2b1409a44f4ab10a399cc0151bdcc874d
Nicolas Morche <nickmorche@gmail.com>
2022-12-29 17:31:05 -0300

    Adicionado botões para adicionar, remover e clonar triangulos

--- script.js
+++ script.js
@@ -5,6 +5,21 @@
  */
 const canvas = $('#canvas');
 var triangleSelected = 0;
+var triangles = [];
+
+const configDraggable = {
+    containment: '#canvas', // define que é dentro do canvas
+    cursor: 'move', // ao mover, seta o cursor pra move
+    start: function(){ // ao começar a mover, trás pra frente os triângulos
+        //console.log(this.style);
+        allBehind();
+        this.style.zIndex = "1";
+    },
+    stop: function(){
+        triangleSelected = this.id;
+        //this.style.zIndex = "1";
+    }
+}
 
 /**
  * Cria o triângulo no quadro
@@ -12,49 +27,57 @@ var triangleSelected = 0;
 function create(triangle){
     var txt = $("<p></p>").text(triangle.text);
 
+    var id = 't' + (triangles.length + 1)
+
     // Cria o elemento div do triângulo
     jQuery('<div>', {
-        id: 't' + triangle.id,
+        id: id,
         class: 'node ui-widget-content',
-    }).append(txt).appendTo('#canvas').draggable({
-        containment: '#canvas', // define que é dentro do canvas
-        cursor: 'move', // ao mover, seta o cursor pra move
-        stop: function(){
-            console.log(this.id);
-            triangleSelected = this.id;
-        }
-    }).dblclick(function(){
+    })
+    .append(txt)
+    .appendTo('#canvas')
+    .draggable(configDraggable)
+    .dblclick(function(){
         this.remove();
     })
+    triangle.id = id;
 
-    // Removido a porra do click. É uma merda. Não funciona direito, não triggera.
-    
-    // .on('click', function(){
-    //     console.log(this);
-    //     return;
-    // })
-    
-    // .click(function(){
-    //     console.log(this);
-    //     //triangleSelected = this.id;
-    //     return;
-    // });
+    triangles.push(triangle);
+};
 
-   // update();
-    
+function allBehind(){
+    $('.node').each(function(index,item){
+        // console.log(index + ' ' + $(this));
+        //console.log(item);
+        // console.log(this.style.zIndex);
+        this.style.zIndex = "0";
+        //$(this).style.zIndex = "0";
+    })
 }
 
-// function update(){
-//     $( function() {
-//         $(".node").draggable({
-//                 containment: '#canvas', // define que é dentro do canvas
-//                 cursor: 'move' // ao mover, seta o cursor pra move
-//             });
-//         } );
-// }
 
 // Criando triângulo
 var triangle = {};
 triangle.text = "Isso é uma ideia boa";
-triangle.id = 1;
 create(triangle);
+
+
+// Ao clicar no botão
+$('#add').click(function(){
+    triangle.text = "Sem Conceito";
+    create(triangle);
+})
+
+$('#del').click(function(){
+    console.log(triangleSelected);
+    $('#' + triangleSelected).remove();
+})
+
+$('#cln').click(function(){
+    var triangle = $('#'+triangleSelected);
+    var text = $('#'+triangleSelected)[0].innerText;
+    var triangle = {
+        text: text + ' 2',
+    }
+    create(triangle);
+})
