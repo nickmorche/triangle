ba0b1aa097a8d72f8ad2a974c7a809449323c7e0
Nicolas Morche <nickmorche@gmail.com>
2022-12-29 14:01:18 -0300

    [FIX] Funcionamento inicial do triângulo

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
  * Variáveis globais
  */
 const canvas = $('#canvas');
+var triangleSelected = 0;
 
 /**
  * Cria o triângulo no quadro
@@ -15,9 +16,42 @@ function create(triangle){
     jQuery('<div>', {
         id: 't' + triangle.id,
         class: 'node ui-widget-content',
-    }).append(txt).appendTo('#canvas').draggable(); 
+    }).append(txt).appendTo('#canvas').draggable({
+        containment: '#canvas', // define que é dentro do canvas
+        cursor: 'move', // ao mover, seta o cursor pra move
+        stop: function(){
+            console.log(this.id);
+            triangleSelected = this.id;
+        }
+    }).dblclick(function(){
+        this.remove();
+    })
+
+    // Removido a porra do click. É uma merda. Não funciona direito, não triggera.
+    
+    // .on('click', function(){
+    //     console.log(this);
+    //     return;
+    // })
+    
+    // .click(function(){
+    //     console.log(this);
+    //     //triangleSelected = this.id;
+    //     return;
+    // });
+
+   // update();
     
 }
+
+// function update(){
+//     $( function() {
+//         $(".node").draggable({
+//                 containment: '#canvas', // define que é dentro do canvas
+//                 cursor: 'move' // ao mover, seta o cursor pra move
+//             });
+//         } );
+// }
 
 // Criando triângulo
 var triangle = {};
